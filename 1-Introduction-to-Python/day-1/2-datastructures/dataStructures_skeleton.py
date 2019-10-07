6d9973ba1745b4fba99aadc000e12b0f0c278831
Timo <tschaefer@mailbox.org>
2019-10-07 08:46:52 +0200

    update

---
+++ 1-Introduction-to-Python/day-1/2-datastructures/dataStructures_skeleton.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 16:42:39 2019
+
+@author: Nicolas Pfeuffer
+"""
+
+####Tuples####
+
+Texample = 
+print(Texample)
+
+Texample2 = 
+print(Texample2)
+
+Texample3 = 
+print(Texample3)
+
+Texample[]
+
+Texample[]
+
+Texample2[]
+
+Texample2.
+
+Texample4 = 1,4,3,4,4,5
+Texample4.
+
+Texample5 = 3.0,4.0,12.0
+dVarA, dVarB, dVarC = 
+
+Texample6 = 4.0,8.0,16.0
+Texample6[] = 
+
+####Lists####
+
+LNumbers = [1,2,3,4,5]
+print(LNumbers)
+
+LVarious = [1, 2, 3.0, "hey", True]
+print(LVarious)
+
+LEmpty = 
+print(LEmpty)
+
+LVarious[]
+
+LVarious.
+
+
+LVarious[]
+
+LVariousPart = LVarious[]
+print(LVariousPart)
+
+LSpotify = ["Charts","Neuheiten","Podcasts & Video Shows", "Entdecken", "Konzerte"]
+
+LSpotify[] = 
+
+LSpotify.
+
+LSpotify.
+print(LSpotify)
+
+LSpotify.pop()
+print(LSpotify)
+
+LSpotify.
+print(LSpotify)
+
+LSpotify.
+LSpotify.
+print(LSpotify)
+
+...(LSpotify)
+
+LSpotify.
+
+LSpotify.
+print(LSpotify)
+LSpotify.
+print(LSpotify)
+
+sTestString = "Lists are awesome and so are Strings!"
+sTestString[3]
+sTestString[0:5] # Play a little! Change the first and the second value.
+len(sTestString)
+
+####Sets####
+
+
+SNumbers = {1,2,3,4,5,1}
+print(SNumbers)
+
+SVarious = {1, 2, 3.0, "hey"}
+print(SVarious)
+
+SEmpty = 
+print(SEmpty)
+
+... SVarious
+
+... SVarious
+
+LtoSet = [1,2,3,5,2,4,12,523,123,21]
+SfromList = set()
+print(SfromList)
+
+LfromSet = list()
+print(LfromSet)
+
+
+
+# in SNumbers, but not in SVarious
+SNumbers ...
+
+# in SNumbers or in SVarious or both
+SNumbers ...
+
+# in SNumbers and in SVarious
+SNumbers ...
+
+# in SNumbers or in SVarious but not in both
+SNumbers ...
+
+####Dictionaries####
+
+DNumbers = {"One":1,"Two":2,"Three":3}
+print(DNumbers)
+
+DNumbers_nKeys = {1:1,2:2,3:3}
+print(DNumbers_nKeys)
+
+DNumbers_sVals = {1:"One",2:"Two",3:"Three"}
+print(DNumbers_sVals)
+
+print(DNumbers_sVals[])
+print(DNumbers_sVals[])
+
+DNumbers_sVals[] = ""
+print(DNumbers_sVals[])
+
+DNumbers_sVals.
+DNumbers_sVals.
+
+print(DNumbers_sVals[])
+
+DSomeDicts = {"DNumbers":DNumbers, "DNumbers_nKeys": DNumbers_nKeys}
+print(DSomeDicts)
+
+DSomeDicts[]
+DSomeDicts.
+
+DSomeDicts[] = 
+print(DSomeDicts)
+
+DNumbers[] = 
+print(DNumbers)
+
+DNumbers.
+print(DNumbers)
+
+
+
+#### Exercises ####
+
+# Create a variable of type list "LExercise" with the contents: "Anna", "Charles", "Peter", "Santa", "Eric"
+
+# Get the index of the element named "Santa"
+
+# Get the element with the index 4
+
+# Get the last element
+
+# Assign a string Variable with the contents of element with index 3, but only get the first three letters from the element!
+
+####
+# Create a variable of type dictionary "DExercise" with the key-value pairs: 4213-"Epic", 532-"Exclusive", 213-"Good", 5321-"Job"
+
+# print the contents of the dictionary.
+
+# Retrieve the list of keys.
+
+# Retrieve the value for the key 532
+
+####
+# Create a list variable "LContainer" with the elements: [231,421,324,646],[21423,634,132],[123,765]
+
+# Create a new variable iExampleMultiplication and assign the value of the multiplication between the 
+# first element of the first list in LContainer and the third element of the second list in LContainer.
+
