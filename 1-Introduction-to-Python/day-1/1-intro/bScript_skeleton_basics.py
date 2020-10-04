9e96d5a583fa28a674a0b640924f33dc0c3eda30
Benjamin <benjaminabdel-karim@MacBook-Pro.local>
2020-10-04 20:00:09 +0200

    Update all data

---
+++ 1-Introduction-to-Python/day-1/1-intro/bScript_skeleton_basics.py
@@ -0,0 +1,100 @@
+# Script for Hello World
+# @author: Benjamin M. Abdel-Karim
+# @since: 2019-07-31
+# @version: 2020-10-04
+
+
+# %% Chars are symbols.
+# In Python, char is short for character.
+# All characters in all languages can be represented.
+# This representation is in the Unicode format.
+# Unicode is a computer encoding methodology that assigns
+# a unique number for every character.
+# It doesn't matter what language, or computer platform it's on.
+# Lets try to allocate a char to a variable and get the unicode number.
+# @code: ord() Given a string representing one Unicode character
+# @source: https://docs.python.org/3/library/functions.html
+# @source: https://www.codetable.net/decimal/65
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %% Integer
+# int (signed integer) − They are often called just integers or ints,
+# are positive or negative whole numbers with no decimal point.
+
+
+
+
+# Float (floating point real values, double) − Also called floats,
+# they represent real numbers and are written with a
+# decimal point dividing the integer and fractional parts.
+# Floats may also be in scientific notation, with E
+# or e indicating the power of 10.
+# @code: We use a small d for double.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Range of numbers
+# Find the max values in the range of ints.
+# @code: This module provides access to some
+# variables used or maintained by the interpreter
+# and to functions that interact strongly
+# with the interpreter. It is always available.
+
+
+
+
+
+
+
+
+
+
+
+# %% Boolean
+# Boolean values are the two constant objects False and True.
+# They are used to represent truth values (false or true).
+
+
+
+
+
+
+
