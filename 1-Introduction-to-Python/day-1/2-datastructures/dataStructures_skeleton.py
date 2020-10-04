b549c6e31fe7aeaf4864ba6f77f39b4a0daba7ad
NicPDev <pfeuffer@wiwi.uni-frankfurt.de>
2020-10-04 19:42:31 +0200

    NP Slides - Cleaned and Updated

--- 1-Introduction-to-Python/day-1/2-datastructures/dataStructures_skeleton.py
+++ 1-Introduction-to-Python/day-1/2-datastructures/dataStructures_skeleton.py
@@ -1,169 +1,188 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  1 16:42:39 2019
 
 @author: Nicolas Pfeuffer
+
+#########################################################################################################
+#																										#
+# Do not forget: This Notebook is split into "Code Cells" with the command #%%.							#
+# You can run these cells with the keyboard combo: Strg/Ctrl + Enter	 								#
+# You can also use #%% to create a new separate cell, if you need it.									#
+#																										#
+#########################################################################################################
 """
 
 ####Tuples####
 
 Texample = 
 print(Texample)
-
+#%%
 Texample2 = 
 print(Texample2)
-
+#%%
 Texample3 = 
 print(Texample3)
-
+#%%
 Texample[]
-
+#%%
 Texample[]
-
+#%%
 Texample2[]
-
+#%%
 Texample2.
-
+#%%
 Texample4 = 1,4,3,4,4,5
 Texample4.
-
+#%%
 Texample5 = 3.0,4.0,12.0
 dVarA, dVarB, dVarC = 
-
+#%%
 Texample6 = 4.0,8.0,16.0
 Texample6[] = 
-
+#%%
 ####Lists####
 
 LNumbers = [1,2,3,4,5]
 print(LNumbers)
-
+#%%
 LVarious = [1, 2, 3.0, "hey", True]
 print(LVarious)
-
+#%%
 LEmpty = 
 print(LEmpty)
-
+#%%
 LVarious[]
-
+#%%
 LVarious.
-
+#%%
 
 LVarious[]
-
+#%%
 LVariousPart = LVarious[]
 print(LVariousPart)
-
+#%%
 LSpotify = ["Charts","Neuheiten","Podcasts & Video Shows", "Entdecken", "Konzerte"]
 
 LSpotify[] = 
-
+#%%
 LSpotify.
-
+#%%
 LSpotify.
 print(LSpotify)
-
+#%%
 LSpotify.pop()
 print(LSpotify)
-
+#%%
 LSpotify.
 print(LSpotify)
-
+#%%
 LSpotify.
+#%%
 LSpotify.
 print(LSpotify)
-
+#%%
 ...(LSpotify)
-
+#%%
 LSpotify.
-
-LSpotify.
-print(LSpotify)
+#%%
 LSpotify.
 print(LSpotify)
-
+#%%
+LSpotify.
+print(LSpotify)
+#%%
 sTestString = "Lists are awesome and so are Strings!"
 sTestString[3]
 sTestString[0:5] # Play a little! Change the first and the second value.
 len(sTestString)
-
+#%%
 ####Sets####
 
 
 SNumbers = {1,2,3,4,5,1}
 print(SNumbers)
-
+#%%
 SVarious = {1, 2, 3.0, "hey"}
 print(SVarious)
-
+#%%
 SEmpty = 
 print(SEmpty)
-
+#%%
 ... SVarious
-
+#%%
 ... SVarious
-
+#%%
 LtoSet = [1,2,3,5,2,4,12,523,123,21]
 SfromList = set()
 print(SfromList)
-
+#%%
 LfromSet = list()
 print(LfromSet)
-
+#%%
 
 
 # in SNumbers, but not in SVarious
 SNumbers ...
-
+#%%
 # in SNumbers or in SVarious or both
 SNumbers ...
-
+#%%
 # in SNumbers and in SVarious
 SNumbers ...
-
+#%%
 # in SNumbers or in SVarious but not in both
 SNumbers ...
-
+#%%
 ####Dictionaries####
 
 DNumbers = {"One":1,"Two":2,"Three":3}
 print(DNumbers)
-
+#%%
 DNumbers_nKeys = {1:1,2:2,3:3}
 print(DNumbers_nKeys)
-
+#%%
 DNumbers_sVals = {1:"One",2:"Two",3:"Three"}
 print(DNumbers_sVals)
-
+#%%
 print(DNumbers_sVals[])
 print(DNumbers_sVals[])
-
+#%%
 DNumbers_sVals[] = ""
 print(DNumbers_sVals[])
-
+#%%
 DNumbers_sVals.
+#%%
 DNumbers_sVals.
-
+#%%
 print(DNumbers_sVals[])
-
+#%%
 DSomeDicts = {"DNumbers":DNumbers, "DNumbers_nKeys": DNumbers_nKeys}
 print(DSomeDicts)
-
+#%%
 DSomeDicts[]
 DSomeDicts.
-
+#%%
 DSomeDicts[] = 
 print(DSomeDicts)
-
+#%%
 DNumbers[] = 
 print(DNumbers)
-
+#%%
 DNumbers.
 print(DNumbers)
+#%%
 
-
-
+###################
 #### Exercises ####
+###################
+
+#########################################################################################################
+#																										#
+# Do not forget: Insert #%% to create a new separate cell, which you can run with Strg/Ctrl + Enter		#
+#																										#
+#########################################################################################################
+
+#%%
 
 # Create a variable of type list "LExercise" with the contents: "Anna", "Charles", "Peter", "Santa", "Eric"
 
